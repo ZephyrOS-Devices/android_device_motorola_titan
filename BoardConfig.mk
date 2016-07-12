3af4fe032438b54931f56164fc45801b64adf10f
Vidhanth Bafna <vidhanthbafna@gmail.com>
2016-07-12 17:57:23 +0530

    No need of this

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,14 +32,6 @@ TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 # Kernel
 TARGET_KERNEL_CONFIG := titan_defconfig
 
-#Kernel Toolchain
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-5.x/bin
-
-KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
-
-# Rom Toolchain
-TARGET_GCC_VERSION_EXP := 5.x
-
 # Optimization
 CLANG_O3 := true
 STRICT_ALIASING := false
